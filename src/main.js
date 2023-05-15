f06e0e8c16b072a692cacaac35d07d7be363ee46
Jesus Zuñiga <jesuszt@hotmail.com>
2023-05-15 11:49:35 -0600

    Funciones de flecha y anónimas

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ function funcionSuma(a, b) {
     return a+b;
 }
 
+//Forma función anónima.
+const numeroAleatorio = function(minimo, maximo) {
+    return faker.number.float({min: minimo, max: maximo});
+}
+
 //Función de flecha, forma más usada.
 const funcionHola = (nombre = 'Jesús', apellidoPaterno = 'Zúñiga', apellidoMaterno = 'Trejo', edad = 0) => {
-    const saludo = `Hola ${nombre} ${apellidoPaterno} ${apellidoMaterno}, edad: ${edad}`;
-    return saludo;
+    return `Hola ${nombre} ${apellidoPaterno} ${apellidoMaterno}, edad: ${edad}`;
+}
+
+const fib = (n) => {
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
 }
 
 const primerNombre = faker.person.firstName();
@@ -20,6 +29,12 @@ const resultadoHola = funcionHola(primerNombre,paterno,materno,edad);
 const primerSumando = faker.number.int();
 const segundoSumando = faker.number.int();
 
+const enteroAletorio = faker.number.int({min:1,max:10});
+
 console.log(`Resultado de funcionHola(): ${resultadoHola}`);
 
 console.log(`Resultado de ${primerSumando} + ${segundoSumando} = ${funcionSuma(primerSumando,segundoSumando)}`);
+
+console.log(`Generar número real aleatorio entre 0 y 100: ${numeroAleatorio(0,100)}`);
+
+console.log(`Número de Fibonacci de ${enteroAletorio} = ${fib(enteroAletorio)}`);
