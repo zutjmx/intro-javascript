0aa0d7bd973cb97643bafd2ff5aa9cc522c6bd47
Jesus Zuñiga <jesuszt@hotmail.com>
2023-05-17 17:57:29 -0600

    Método map para arreglos

--- src/main.js
+++ src/main.js
@@ -1,21 +1,103 @@
 //Faker para simular datos
 import { faker } from "@faker-js/faker/locale/es_MX";
 
-//Desestructuración de arreglos
-//Se crea arreglo de nombres de usuario
-let usuarios = [];
+//Arreglo método Map
+//Se crea un arreglo de números enteros aleatorios.
+let numerosEnteros = [];
 for (let index = 0; index < 10; index++) {
-    const element = faker.internet.userName();
-    usuarios.push(element);
+    const elemento = faker.number.int({min:1,max:100});
+    numerosEnteros.push(elemento);
 }
-console.log('Usuarios',usuarios);
 
-//Des estructurar los elementos del arreglo en variables
-const [usuario1,usuario2,usuario3,usuario4,usuario5,,,,usuario9] = usuarios;
+console.log(`numerosEnteros ${numerosEnteros}`);
 
-console.log(`usuario1: ${usuario1}`);
-console.log(`usuario2: ${usuario2}`);
-console.log(`usuario3: ${usuario3}`);
-console.log(`usuario4: ${usuario4}`);
-console.log(`usuario5: ${usuario5}`);
-console.log(`usuario9: ${usuario9}`);
+var dobles = numerosEnteros.map(function(x) {
+    return x * 2;
+});
+console.log(`dobles ${dobles}`);
+
+//Función para generar objeto cliente.
+const generaCliente = () => {
+    const objetoCliente = {
+        id: faker.number.int({min:1,max:1000}),
+        nombre: faker.person.firstName(),
+        aPaterno: faker.person.lastName(),
+        aMaterno: faker.person.middleName(),
+        fechaNacimiento: faker.date.birthdate({min:18,max:80,mode:'age'}),
+    }
+    return objetoCliente;
+}
+
+//Función para crear un arreglo de objetos para la propiedad items
+const generaItems = () => {
+    let itemArray = [];
+    for (var i = 0; i < 20; i++) {
+        const item = {
+            id: faker.number.int({min:1,max:1000}),
+            descripcion: faker.commerce.product(),
+            cantidad: faker.number.int({min:1,max:10}),
+            precio: faker.number.float({min:1,max:2000,precision:0.01}),
+        }
+        itemArray.push(item);
+    }
+    return itemArray;
+}
+
+//Función para generar cadenas para la propiedad descripción.
+const generaDescripcion = () => {
+    return faker.company.catchPhrase();
+}
+
+//Arreglo de objetos de tipo factura
+const facturas = [
+    {
+        id: faker.number.int({min:1,max:1000}),
+        fecha: faker.date.anytime(),
+        descripcion: generaDescripcion(),
+        cliente: generaCliente(),
+        items: generaItems(),
+        direccion: faker.location.streetAddress(),
+        email: faker.internet.email()
+    },
+    {
+        id: faker.number.int({min:1,max:1000}),
+        fecha: faker.date.anytime(),
+        descripcion: generaDescripcion(),
+        cliente: generaCliente(),
+        items: generaItems(),
+        direccion: faker.location.streetAddress(),
+        email: faker.internet.email()
+    },
+    {
+        id: faker.number.int({min:1,max:1000}),
+        fecha: faker.date.anytime(),
+        descripcion: generaDescripcion(),
+        cliente: generaCliente(),
+        items: generaItems(),
+        direccion: faker.location.streetAddress(),
+        email: faker.internet.email()
+    },
+    {
+        id: faker.number.int({min:1,max:1000}),
+        fecha: faker.date.anytime(),
+        descripcion: generaDescripcion(),
+        cliente: generaCliente(),
+        items: generaItems(),
+        direccion: faker.location.streetAddress(),
+        email: faker.internet.email()
+    }
+];
+
+console.log('Arreglo de facturas: ', facturas);
+
+const descripcionesfacturas = facturas.map(factura => {
+    return factura.descripcion;
+});
+
+console.log('Solo descripciones de las facturas: ', descripcionesfacturas);
+
+const nombresClientesfacturas = facturas.map(factura => {
+    return factura.cliente.nombre;
+});
+
+console.log('Solo nombres de clientes de las facturas: ', nombresClientesfacturas);
